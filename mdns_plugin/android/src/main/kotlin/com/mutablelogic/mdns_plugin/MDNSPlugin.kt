360d6933f90046888d260a17bbc91fd7c2e546b1
Tiago Alves <letiagoalves@gmail.com>
2021-01-06 01:56:32 +0000

    move to new Android plugins API

--- mdns_plugin/android/src/main/kotlin/com/mutablelogic/mdns_plugin/MDNSPlugin.kt
+++ mdns_plugin/android/src/main/kotlin/com/mutablelogic/mdns_plugin/MDNSPlugin.kt
@@ -1,57 +1,78 @@
 package com.mutablelogic.mdns_plugin
 
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.EventChannel
-import io.flutter.plugin.common.EventChannel.StreamHandler
-import io.flutter.plugin.common.EventChannel.EventSink
-import io.flutter.plugin.common.PluginRegistry.Registrar
+import android.app.Activity
 import android.content.Context
 import android.net.nsd.NsdManager
 import android.net.nsd.NsdServiceInfo
 import android.util.Log
-import android.app.Activity
-import java.util.Timer
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.EventChannel.EventSink
+import io.flutter.plugin.common.EventChannel.StreamHandler
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.collections.set
 import kotlin.concurrent.schedule
 
-class MDNSPlugin : MethodCallHandler,StreamHandler {
-  var nsdManager: NsdManager? = null
+
+class MDNSPlugin : FlutterPlugin, MethodCallHandler, StreamHandler, ActivityAware {
+  private var applicationContext: Context? = null
+  private var methodChannel: MethodChannel? = null
+  private var eventChannel: EventChannel? = null
+  private var nsdManager: NsdManager? = null
+
+  var discoveryListener: DiscoveryListener? = null
   var sink: EventSink? = null
   var activity: Activity? = null
-  var discoveryListener: DiscoveryListener? = null
-  val services : HashMap<String,NsdServiceInfo> = HashMap<String,NsdServiceInfo>()
+  val services : HashMap<String, NsdServiceInfo> = HashMap()
 
   companion object {
+    /*
     @JvmStatic
     fun registerWith(registrar: Registrar) {
-      MethodChannel(registrar.messenger(), "mdns_plugin").setMethodCallHandler(MDNSPlugin(registrar))      
+      MethodChannel(registrar.messenger(), "mdns_plugin").setMethodCallHandler(MDNSPlugin(registrar))
     }
+    */
 
-    fun mapFromServiceInfo(method: String,serviceInfo: NsdServiceInfo?):HashMap<String,Any> {
+    fun mapFromServiceInfo(method: String, serviceInfo: NsdServiceInfo?):HashMap<String, Any> {
       val map = HashMap<String, Any>()
       map["method"] = method
       serviceInfo?.let {
-        map["name"] = serviceInfo.getServiceName()
-        map["type"] = serviceInfo.getServiceType().removePrefix(".").removeSuffix(".") + "." // cleanup
-        map["hostName"] = serviceInfo.getHost()?.getHostName() ?: ""
-        map["port"] = serviceInfo.getPort()
-        map["txt"] = serviceInfo.getAttributes()
+        map["name"] = serviceInfo.serviceName
+        map["type"] = serviceInfo.serviceType.removePrefix(".").removeSuffix(".") + "." // cleanup
+        map["hostName"] = serviceInfo.host?.hostName ?: ""
+        map["port"] = serviceInfo.port
+        map["txt"] = serviceInfo.attributes
 
-        val addr = serviceInfo.getHost()?.getHostAddress()
+        val addr = serviceInfo.host?.hostAddress
         addr?.let {
-          map["addr"] = listOf(addr,serviceInfo.getPort())
+          map["addr"] = listOf(addr, serviceInfo.port)
         }
       }
       return map
     }
   }
 
-  constructor(registrar: Registrar) {
-    nsdManager = registrar.activity().getSystemService(Context.NSD_SERVICE) as NsdManager
-    activity = registrar.activity()
-    EventChannel(registrar.messenger(), "mdns_plugin_delegate").setStreamHandler(this)
+  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    this.applicationContext = binding.applicationContext;
+    this.methodChannel = MethodChannel(binding.binaryMessenger, "mdns_plugin")
+    this.eventChannel = EventChannel(binding.binaryMessenger, "mdns_plugin_delegate")
+    this.eventChannel!!.setStreamHandler(this);
+    this.methodChannel!!.setMethodCallHandler(this)
+  }
+
+  override fun onDetachedFromEngine(p0: FlutterPlugin.FlutterPluginBinding) {
+    this.applicationContext = null;
+    this.methodChannel?.setMethodCallHandler(null);
+    this.methodChannel = null;
+    this.eventChannel?.setStreamHandler(null);
+    this.eventChannel = null;
   }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
@@ -60,13 +81,15 @@ class MDNSPlugin : MethodCallHandler,StreamHandler {
         result.success("Android ${android.os.Build.VERSION.RELEASE}")
       }
       "startDiscovery" -> {
-        startDiscovery(result,call.argument<String>("serviceType") ?: "",call.argument<Boolean>("enableUpdating") ?: false)
+        startDiscovery(result, call.argument<String>("serviceType")
+                ?: "", call.argument<Boolean>("enableUpdating") ?: false)
       }
       "stopDiscovery" -> {
         stopDiscovery(result)
       }
       "resolveService" -> {
-        resolveService(result,call.argument<String>("name") ?: "",call.argument<Boolean>("resolve") ?: false)
+        resolveService(result, call.argument<String>("name")
+                ?: "", call.argument<Boolean>("resolve") ?: false)
       }
       else -> {
         result.notImplemented()
@@ -74,7 +97,7 @@ class MDNSPlugin : MethodCallHandler,StreamHandler {
     }
   }
 
-  override fun onListen(p0: Any?, sink: EventSink) {   
+  override fun onListen(p0: Any?, sink: EventSink) {
     this.sink = sink
   }
 
@@ -82,7 +105,7 @@ class MDNSPlugin : MethodCallHandler,StreamHandler {
     this.sink = null
   }
 
-  private fun startDiscovery(result: Result,serviceType: String,enableUpdating: Boolean) {
+  private fun startDiscovery(result: Result, serviceType: String, enableUpdating: Boolean) {
     if(enableUpdating) {
       Log.w("MDNSPlugin", "startDiscovery: enableUpdating is currently ignored on the Android platform")
     }
@@ -102,10 +125,10 @@ class MDNSPlugin : MethodCallHandler,StreamHandler {
     result.success(null);
   }
 
-  private fun resolveService(result: Result,name: String, resolve: Boolean) {
+  private fun resolveService(result: Result, name: String, resolve: Boolean) {
     if(services.containsKey(name)) {
       if(resolve) {
-        nsdManager?.resolveService(services.get(name),ResolveListener(this));
+        nsdManager?.resolveService(services.get(name), ResolveListener(this));
       } else {
         services.remove(name)
       }
@@ -113,11 +136,31 @@ class MDNSPlugin : MethodCallHandler,StreamHandler {
       Log.w("MDNSPlugin", "resolveService: missing service with name $name")
     }
   }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    this.activity = binding.activity;
+    this.nsdManager = binding.activity.getSystemService(Context.NSD_SERVICE) as NsdManager
+  }
+
+  override fun onDetachedFromActivity() {
+    this.activity = null;
+    this.nsdManager = null;
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    this.activity = null;
+    this.nsdManager = null;
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    this.activity = binding.activity;
+    this.nsdManager = binding.activity.getSystemService(Context.NSD_SERVICE) as NsdManager
+  }
 }
 
-class ResolveListener(val plugin: MDNSPlugin) : NsdManager.ResolveListener {
+class ResolveListener(private val plugin: MDNSPlugin) : NsdManager.ResolveListener {
     override fun onServiceResolved(serviceInfo: NsdServiceInfo) {
-      val serviceMap = MDNSPlugin.mapFromServiceInfo("onServiceResolved",serviceInfo)
+      val serviceMap = MDNSPlugin.mapFromServiceInfo("onServiceResolved", serviceInfo)
       plugin.activity?.runOnUiThread(java.lang.Runnable {
         plugin.sink?.success(serviceMap)
       })
@@ -126,7 +169,7 @@ class ResolveListener(val plugin: MDNSPlugin) : NsdManager.ResolveListener {
       when (errorCode) {
         NsdManager.FAILURE_ALREADY_ACTIVE -> {
           // Resolve again after a short delay
-          Timer("resolve", false).schedule(20) { 
+          Timer("resolve", false).schedule(20) {
             plugin.discoveryListener?.onServiceFound(serviceInfo)
           }
         }
@@ -136,7 +179,7 @@ class ResolveListener(val plugin: MDNSPlugin) : NsdManager.ResolveListener {
     }
 }
 
-class DiscoveryListener(val plugin: MDNSPlugin) : NsdManager.DiscoveryListener {
+class DiscoveryListener(private val plugin: MDNSPlugin) : NsdManager.DiscoveryListener {
   override fun onDiscoveryStarted(serviceType: String?) {
     plugin.activity?.runOnUiThread(java.lang.Runnable {
       plugin.sink?.success(mapOf("method" to "onDiscoveryStarted"))
@@ -148,18 +191,18 @@ class DiscoveryListener(val plugin: MDNSPlugin) : NsdManager.DiscoveryListener {
     })
   }
   override fun onServiceFound(serviceInfo: NsdServiceInfo) {
-    val serviceMap = MDNSPlugin.mapFromServiceInfo("onServiceFound",serviceInfo)
-    val name = serviceInfo.getServiceName()
+    val serviceMap = MDNSPlugin.mapFromServiceInfo("onServiceFound", serviceInfo)
+    val name = serviceInfo.serviceName
     name?.let {
-      plugin.services.put(name,serviceInfo)
+      plugin.services.put(name, serviceInfo)
     }
     plugin.activity?.runOnUiThread(java.lang.Runnable {
       plugin.sink?.success(serviceMap)
     })
   }
   override fun onServiceLost(serviceInfo: NsdServiceInfo) {
-    val serviceMap = MDNSPlugin.mapFromServiceInfo("onServiceRemoved",serviceInfo)
-    plugin.services.remove(serviceInfo.getServiceName())
+    val serviceMap = MDNSPlugin.mapFromServiceInfo("onServiceRemoved", serviceInfo)
+    plugin.services.remove(serviceInfo.serviceName)
     plugin.activity?.runOnUiThread(java.lang.Runnable {
       plugin.sink?.success(serviceMap)
     })
